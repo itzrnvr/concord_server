858c60a2f9dde48060e47ca30c1855210ce4c9a0
Anatame <anata.me97@gmail.com>
2022-12-19 14:22:49 +0530

    ping head request

--- server/home/__init__.py
+++ server/home/__init__.py
@@ -3,8 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+@router.head("/ping")
+async def ping():
+    return "OK"
+
+
 @router.get("/")
 async def home():
     return "Yo brah"
-
-
--- server/paths.py
+++ server/paths.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from .home import router as homeRouter
-from .authenticate import router as authRouter
-from .chat import router as chatRouter
+from server.home import router as homeRouter
+from server.authenticate import router as authRouter
+from server.chat import router as chatRouter
 
 
